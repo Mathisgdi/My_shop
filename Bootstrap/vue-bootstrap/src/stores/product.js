d89a9b4c32f7b018286a7801e52be851b0fea697
Mathisgdi <mathis.ghidotti@epitech.digital>
2023-11-14 23:00:46 +0100

    Get product name and description
    Add GET product name and description

--- Bootstrap/vue-bootstrap/src/stores/product.js
+++ Bootstrap/vue-bootstrap/src/stores/product.js
@@ -2,17 +2,18 @@ import { defineStore } from 'pinia'
 import  axios  from  'axios'
 
 // Configure Axios avec un intercepteur pour l'authentification
-axios.interceptors.request.use(config => {
-  // Ajoute le token d'authentification à chaque requête sortante
-  config.headers.Authorization = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2OTk5NzAxMjUsImV4cCI6MTY5OTk3MzcyNSwicm9sZXMiOlsiUk9MRV9BRE1JTiIsIlJPTEVfVVNFUiJdLCJ1c2VybmFtZSI6Im1hdGJkeDMzMzhAZ21haWwuY29tIn0.aBhyokUuIH1Sbp0jlSyY6fG3trKThYUDW1gItT5OIFLStI0Gr2GVWHluPZVkh7NHnqpsQe2H6FanOJ5ScpDT3d6T0flHjuTuhmxEqhnU6E2FGtAbsVDxhPzInhtr_jUoCQbJYPuFubrUs7j5hp8eFXLJ-xnD33jKWQzs_38Q8R4OZDg03Cv57mMCLAYyFEVbx9H6BEuK2NSwZtt8rXHctEDlsoEBKhU7EMlqNbo0LezdF6mnTieLkIKyD6_lDs6vI4LJtQSdo7zycvPirHsIbdYDAVxOMVrxda1qTLrr8esfwNMHnT2S89UJ4xWVhBg1GxELa7tLzRh3SHW3_bIEPA';
-  return config;
-});
+// axios.interceptors.request.use(config => {
+//   // Ajoute le token d'authentification à chaque requête sortante
+//   config.headers.Authorization = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2OTk5NzAxMjUsImV4cCI6MTY5OTk3MzcyNSwicm9sZXMiOlsiUk9MRV9BRE1JTiIsIlJPTEVfVVNFUiJdLCJ1c2VybmFtZSI6Im1hdGJkeDMzMzhAZ21haWwuY29tIn0.aBhyokUuIH1Sbp0jlSyY6fG3trKThYUDW1gItT5OIFLStI0Gr2GVWHluPZVkh7NHnqpsQe2H6FanOJ5ScpDT3d6T0flHjuTuhmxEqhnU6E2FGtAbsVDxhPzInhtr_jUoCQbJYPuFubrUs7j5hp8eFXLJ-xnD33jKWQzs_38Q8R4OZDg03Cv57mMCLAYyFEVbx9H6BEuK2NSwZtt8rXHctEDlsoEBKhU7EMlqNbo0LezdF6mnTieLkIKyD6_lDs6vI4LJtQSdo7zycvPirHsIbdYDAVxOMVrxda1qTLrr8esfwNMHnT2S89UJ4xWVhBg1GxELa7tLzRh3SHW3_bIEPA';
+//   return config;
+// });
 
 export const useProductStore = defineStore('product', {
   state : () => {
     return{
       product : [],
       status  : 'init'
+      
     }
   },
   getters : {
--- Bootstrap/vue-bootstrap/src/views/Home.vue
+++ Bootstrap/vue-bootstrap/src/views/Home.vue
@@ -1,20 +1,12 @@
 <template>
 <section>
     <h1>Home</h1>
-    <p>Home page cogntentt</p>
+    <h1>Voici les noms de mes produits avec leur description:</h1>
     <section v-if="getStatus == 'done' "> <!-- si mon status est done alors on affiche les éléments -->
-        
-        <!-- <article v-for = "product in getProduct.data['hydra:member']" :key="product.id"> 
-            <h1>{{ getProduct.data['hydra:member'] }}</h1>
-            
-        </article> -->
-        <!-- <article v-for = "product in getProduct.data+'hydra:member.description'" :key="product.id"> 
-            <h1>{{ getProduct.data }}</h1>
-            
-        </article> -->
         <article v-for="product in getProduct.data['hydra:member']" :key="product.id"> 
-    <h1>{{ product.name }}</h1>
-</article>
+            <h1> {{ product.name }}</h1>
+            <h1> {{ product.description }}</h1>
+        </article>
     </section>
     <section v-else> <!-- sinon on affiche un message de chargement -->
         ...Loading
@@ -34,21 +26,22 @@ export default{
     data() { // C'est ici qu'on déclare les variables
         return {
             message: "Hello World",
-            pokemons : [],
+            product : [],
             // text: "", // on déclare une variable text qui va être lié à l'input
         };
     },
-    mounted(){ //instruction lancer avant le chargement de la page
-        this.fetchProduct(); // lance ma requete pour récupérer les pokemons
-    },
+   
+
+    async mounted() {
+    await this.fetchProduct;
+  // Continuer avec d'autres actions
+},
     computed: { 
         ...mapActions(useProductStore,["fetchProduct"]), // Va importer les actions qui sont dans le store (ici on a que fetchPokemons)
         ...mapState(useProductStore,["getProduct","getStatus"]) // Va importer les states (getter) qui sont dans le store (ici on a que pokemons)
     },
     method: { // C'est ici qu'on déclare les fonctions
-        multply(arg1, arg2){
-            return arg1 * arg2;
-        }
+        
     },
 }
 
