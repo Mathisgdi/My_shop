bbbbfc3d5944a2a6465a5dd2ce8a1e2c31834230
Mathisgdi <mathis.ghidotti@epitech.digital>
2023-11-15 15:00:54 +0100

    add get product in test.vue

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
 
 Faire npm install pour installer tous les modules nécessaeires au bon fonctionnement du projet.
-
+installer axios avec la commande `npm install axios`
 Npm run dev pour lancer le serveur.
--- my-shop-api/vue-MyShop/src/views/Test.vue
+++ my-shop-api/vue-MyShop/src/views/Test.vue
@@ -1,15 +1,55 @@
 <template>
     <section>
-        <p> test </p>
-        <RouterLink to="/"> Home</RouterLink>
+        <h1>Home</h1>
+        <h1>Voici les noms de mes produits avec leur description:</h1>
+        <section v-if="getStatus == 'done' "> <!-- si mon status est done alors on affiche les éléments -->
+            <article v-for="product in getProduct.data['hydra:member']" :key="product.id"> 
+                <h1> {{ product.name }}</h1>
+                <h1> {{ product.description }}</h1>
+            </article>
+        </section>
+        <section v-else> <!-- sinon on affiche un message de chargement -->
+            ...Loading
+        </section>
+    
     </section>
-</template>
-
-<script>
-
-</script>
-
-
-<style>
-
-</style>
+    
+    
+    </template>
+    
+    <script>
+    import { mapActions, mapState } from "pinia";
+    import {useProductStore} from '../stores/product'
+    
+    
+    export default{
+        data() { // C'est ici qu'on déclare les variables
+            return {
+                message: "Hello World",
+                product : [],
+                // text: "", // on déclare une variable text qui va être lié à l'input
+            };
+        },
+       
+    
+        async mounted() {
+        await this.fetchProduct;
+      // Continuer avec d'autres actions
+            },
+        computed: { 
+            ...mapActions(useProductStore,["fetchProduct"]), // Va importer les actions qui sont dans le store (ici on a que fetchPokemons)
+            ...mapState(useProductStore,["getProduct","getStatus"]) // Va importer les states (getter) qui sont dans le store (ici on a que pokemons)
+        },
+        method: { // C'est ici qu'on déclare les fonctions
+            
+        },
+    }
+    
+    
+    </script>
+    
+    
+    
+    <style>
+    
+    </style>
